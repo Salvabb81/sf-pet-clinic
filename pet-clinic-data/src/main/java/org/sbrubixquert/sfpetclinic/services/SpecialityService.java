77b3dfd5bfff2409cbfafea915d14987af626745
Salvabb81 <salvabb81@users.noreply.github.com>
2020-10-26 09:40:01 +0100

    Creating Speciality Service and Service Map Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/org/sbrubixquert/sfpetclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package org.sbrubixquert.sfpetclinic.services;
+
+import org.sbrubixquert.sfpetclinic.model.Speciality;
+
+public interface SpecialityService extends CommonService<Speciality, Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/org/sbrubixquert/sfpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,37 @@
+package org.sbrubixquert.sfpetclinic.services.map;
+
+import java.util.Set;
+
+import org.sbrubixquert.sfpetclinic.model.Speciality;
+import org.sbrubixquert.sfpetclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+	@Override
+	public Set<Speciality> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Speciality findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Speciality save(Speciality object) {
+		return super.save(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Speciality object) {
+		super.delete(object);
+	}
+
+}
